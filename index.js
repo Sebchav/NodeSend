8ba7513c54e57c316e6964bd40310ede9bc12045
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-08-26 14:08:20 -0600

    Connecting to NodeSend database and the Users Routing

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const conectarDB = require("./config/db");
 
 // Crear el servidor
 const app = express();
 
-console.log("Comenzando Node Send")
+//Conectar a la base de datos de NodeSend
+conectarDB();
+
 //Puerto de la app
 
 const port = process.env.PORT || 4000;
+
+//Rutas de la app
+app.use("/api/usuarios", require("./routes/usuarios"));
 
 //Arrancar app
 app.listen(port, '0.0.0.0', ()=>{
