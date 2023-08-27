4d41c08a21ef811e255f97071901f19f0489863b
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-08-27 15:28:57 -0600

    Creating JSON Web Token and adding express validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,15 @@
 const Usuario = require("../models/Usuario");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+require("dotenv").config({ path: "variables.env" });
+const { validationResult } = require("express-validator");
 
 exports.autenticarUsuario = async(req, res, next) => {
-    //Revisar si hay errores
-
+    //Mostrar mensajes de Error express validator
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()});
+    }
 
     //Buscar el usuario
     const { email, password } = req.body;
@@ -18,7 +24,15 @@ exports.autenticarUsuario = async(req, res, next) => {
     //Verificar el password y autenticar el usuario
 
     if(bcrypt.compareSync(password, usuario.password)){
-        //Crear JWT
+        const token = jwt.sign({
+            id: usuario._id,
+            nombre: usuario.nombre
+        }, process.env.SECRETA, {
+            expiresIn: "8h"
+        });
+        
+        res.json({token});
+
     }else{
         res.status(401).json({msg: "Password incorrecto"});
         return next();
