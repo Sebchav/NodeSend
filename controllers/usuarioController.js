d75800a7be9c062b4aa63c543231e3c463d04a5c
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-08-26 18:42:18 -0600

    Avoid duplicated users, Hashing password, Adding express validator

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,8 +1,36 @@
 const Usuario = require("../models/Usuario");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 
 exports.nuevoUsuario = async(req, res) => {
-    const usuario = await new Usuario(req.body);
-    usuario.save();
+
+    //Mostrar mensajes de Error express validator
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()});
+    }
+
+    //Verificar si el usuario ya estuvo registrado
+    const { email, password } = req.body;
+
+    let usuario = await Usuario.findOne({ email });
+
+    if(usuario){
+        return res.status(400).json({ msg: "El Usuario ya está registrado "});
+    }
+
+    //Crear un nuevo usuario
+    usuario = new Usuario(req.body);
     
-    res.json({msg: "Usuario Creado Correctamente"});
+    //Hashear el password
+    const salt = await bcrypt.genSalt(10);
+    usuario.password = await bcrypt.hash(password, salt);
+    
+    try{
+        await usuario.save();
+
+        res.json({msg: "Usuario Creado Correctamente"});
+    }catch(error){
+        console.log(error);
+    }   
 }
