c9d9094a6659d7a09029e25831941fceb1b0f857
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-08-28 19:19:18 -0600

    Getting the authenticated user with JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,5 +41,22 @@ exports.autenticarUsuario = async(req, res, next) => {
 }
 
 exports.usuarioAutenticado = (req, res)=>{
+    const authHeader = req.get("Authorization");
 
+    if(authHeader){
+        //Obtener el Token
+        const token = authHeader.split(" ")[1];
+
+        //Comprobar JWT
+        try{
+            const usuario = jwt.verify(token, process.env.SECRETA);
+            
+            res.json({usuario});
+        }catch(error){
+            console.log(error);
+            console.log("JWT no valido");
+        }  
+    } 
+
+    return next();
 }
