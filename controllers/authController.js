f2ed21d888b28608c7b74b6c8c923fdf12a97000
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-09-01 20:39:08 -0600

    Creating auth middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,22 +41,5 @@ exports.autenticarUsuario = async(req, res, next) => {
 }
 
 exports.usuarioAutenticado = (req, res)=>{
-    const authHeader = req.get("Authorization");
-
-    if(authHeader){
-        //Obtener el Token
-        const token = authHeader.split(" ")[1];
-
-        //Comprobar JWT
-        try{
-            const usuario = jwt.verify(token, process.env.SECRETA);
-            
-            res.json({usuario});
-        }catch(error){
-            console.log(error);
-            console.log("JWT no valido");
-        }  
-    } 
-
-    return next();
+    res.json({usuario: req.usuario });
 }
