f24683661aff0161d2b0d1da4ebda5be31d01374
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-09-03 16:14:58 -0600

    Saving Links in the BD, ShortID

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,5 +1,27 @@
+const Enlaces = require("../models/Enlace");
+const shortid = require("shortid");
+
 exports.nuevoEnlace = async(req, res, next) => {
     //Revisar si hay errores
 
-    //Almacenar en la BD
+    //Crear un objeto
+    const { nombre_original, password } = req.body;
+
+    const enlace = new Enlaces();
+    enlace.url = shortid.generate();
+    enlace.nombre = shortid.generate();
+    enlace.nombre_original = nombre_original;
+    enlace.password = password;
+
+    // Si el usuario está autenticado
+    
+
+    // Almacenar en la BD
+    try{
+        await enlace.save();
+        return res.json({msg: `${enlace.url}`});
+        next();
+    }catch(error){
+        console.log(error);
+    }
 }
