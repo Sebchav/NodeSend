5b105a378cf7be7948a1186471d45407c128dff8
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-09-03 16:36:43 -0600

    Defining Downloads number and Password for Links, adding validation

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,20 +1,42 @@
 const Enlaces = require("../models/Enlace");
 const shortid = require("shortid");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 
 exports.nuevoEnlace = async(req, res, next) => {
     //Revisar si hay errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()});
+    }
 
     //Crear un objeto
-    const { nombre_original, password } = req.body;
+    const { nombre_original } = req.body;
 
     const enlace = new Enlaces();
     enlace.url = shortid.generate();
     enlace.nombre = shortid.generate();
     enlace.nombre_original = nombre_original;
-    enlace.password = password;
 
     // Si el usuario está autenticado
-    
+    if(req.usuario) {
+        const { password, descargas } = req.body;
+
+        //Asignar a enlace el numero de descargas 
+        if(descargas){
+            enlace.descargas = descargas;
+        }
+
+        //Asignar un password
+        if(password) {
+            const salt = await bcrypt.genSalt(10);
+
+            enlace.password = await bcrypt.hash( password, salt );
+        }
+        
+        //Asignar el autor
+        enlace.autor = req.usuario.id;
+    }
 
     // Almacenar en la BD
     try{
